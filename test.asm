a2251cb4cce0b31c007e96a899297343b2348d06
wwsmiff <arnav0872@gmail.com>
2023-01-23 19:12:18 +0530

    Added support for comments

--- test.asm
+++ test.asm
@@ -1,11 +1,13 @@
-MOV R1, 20;
-MOV R2, 50;
-ADD R1, $R2;
+# A simple test program to test the operations
+# and getting values from registers
+
+MOV R1, 20; # Moving 20 into R1
+MOV R2, 50; # Moving 50 into R2
+ADD R1, $R2; # Adding the value held by R2 to R1
 RPRINT R1;
+
+# Same thing but for floats
 FMOV FPR1, 5.0;
 FMOV FPR2, 2.0;
 FADD FPR1, $FPR2;
-FPRINT FPR1;
-FMOV FPR1, 5.0;
-FDIV FPR1, $FPR2;
 FPRINT FPR1;
